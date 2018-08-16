c63e14d1f1f5b30467a125d81722b4ceed6e5506
Foysal Ahamed <thisisfoysal@gmail.com>
2018-08-16 16:38:36 +0600

    Added loading bar on game startup

--- src/js/scene/PreloadScene.js
+++ src/js/scene/PreloadScene.js
@@ -7,7 +7,10 @@ class PreloadScene extends Phaser.Scene {
         /* Showing application title */
         let appIcon = this.add.image(SpeedMatch.game.config.width / 2, SpeedMatch.game.config.height / 4, "apptitle");
         appIcon.setOrigin(0.5, 0.5);
-        
+
+        /* Adding and configuring progress bar */
+        this.setupProgressBar();
+
         /* Loading images and sprites */
         this.load.image("yes", "assets/sprites/yes.png");
         this.load.image("no", "assets/sprites/no.png");
@@ -26,5 +29,69 @@ class PreloadScene extends Phaser.Scene {
 
     create() {
         this.scene.start("GameScene");
+    }
+
+    setupProgressBar() {
+        let progressBar = this.add.graphics();
+        let progressBox = this.add.graphics();
+        progressBox.fillStyle(0x34495e, 0.8);
+        progressBox.fillRect(SpeedMatch.game.config.width / 4, SpeedMatch.game.config.height - (SpeedMatch.game.config.height / 4), SpeedMatch.game.config.width / 2, 50);
+
+        let width = this.cameras.main.width / 2;
+        let height = (this.cameras.main.height / 4) * 3;
+
+        /* Initializing text for progress status */
+        let loadingText = this.make.text({
+            x: width,
+            y: height - 30,
+            text: 'Loading...',
+            style: {
+                font: '34px monospace',
+                fill: '#16a085'
+            }
+        });
+        loadingText.setOrigin(0.5, 0.5);
+
+        let percentText = this.make.text({
+            x: width,
+            y: height + 25,
+            text: '0%',
+            style: {
+                font: '30px monospace',
+                fill: '#16a085'
+            }
+        });
+        percentText.setOrigin(0.5, 0.5);
+
+        let assetText = this.make.text({
+            x: width,
+            y: height + 80,
+            text: '',
+            style: {
+                font: '30px monospace',
+                fill: '#16a085'
+            }
+        });
+        assetText.setOrigin(0.5, 0.5);
+
+        /* Load modules callback */
+        this.load.on('progress', function (value) {
+            percentText.setText(parseInt(value * 100) + '%');
+            progressBar.clear();
+            progressBar.fillStyle(0x34495e, 1);
+            progressBar.fillRect(SpeedMatch.game.config.width / 4 + 20, SpeedMatch.game.config.height - (SpeedMatch.game.config.height / 4) + 10, 300 * value, 30);
+        });
+
+        this.load.on('fileprogress', function (file) {
+            assetText.setText('Loading asset: ' + file.key);
+        });
+
+        this.load.on('complete', function () {
+            progressBar.destroy();
+            progressBox.destroy();
+            loadingText.destroy();
+            percentText.destroy();
+            assetText.destroy();
+        });
     }
 }
